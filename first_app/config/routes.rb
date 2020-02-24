35f8204ea4e4bdaf2de37832ab160c2c253484b2
noonespecial009 <noonespecial009@gmail.com>
2020-02-24 14:39:18 -0800

    improve scss

--- first_app/config/routes.rb
+++ first_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'countries/index'
+  root 'countries#index'
+  
   get 'greeter/hello'
   get 'greeter/goodbye'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
